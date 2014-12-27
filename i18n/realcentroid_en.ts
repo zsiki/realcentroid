a244b668c6c43c1a0428b242ec8f548b6d342669
zsiki <siki@agt.bme.hu>
2014-12-27 20:32:58 +0100

    English language file generated

---
+++ i18n/realcentroid_en.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>RealCentroid</name>
+    <message>
+        <location filename="ui_realcentroid.py" line="21"/>
+        <source>RealCentroid</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_realcentroid.py" line="25"/>
+        <source>Polygon layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_realcentroid.py" line="32"/>
+        <source>Output point on surface layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_realcentroid.py" line="40"/>
+        <source>Browse</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_realcentroid.py" line="44"/>
+        <source>Add to map canvas</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_realcentroid.py" line="48"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_realcentroid.py" line="52"/>
+        <source>Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_realcentroid.py" line="56"/>
+        <source>Selected features only</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
