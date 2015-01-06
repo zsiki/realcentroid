9b5b63476946d620ba77cb01140871b0845a99c6
zsiki <siki@agt.bme.hu>
2015-01-06 14:53:27 +0100

    regenerated

--- ui_realcentroid.py
+++ ui_realcentroid.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui_realcentroid.ui'
 #
-# Created: Fri May  9 21:12:44 2014
+# Created: Tue Jan  6 14:51:11 2015
 #      by: PyQt4 UI code generator 4.8.6
 #
 # WARNING! All changes made in this file will be lost!
@@ -17,7 +17,7 @@ except AttributeError:
 class Ui_RealCentroid(object):
     def setupUi(self, RealCentroid):
         RealCentroid.setObjectName(_fromUtf8("RealCentroid"))
-        RealCentroid.resize(389, 198)
+        RealCentroid.resize(389, 233)
         RealCentroid.setWindowTitle(QtGui.QApplication.translate("RealCentroid", "RealCentroid", None, QtGui.QApplication.UnicodeUTF8))
         RealCentroid.setModal(True)
         self.label = QtGui.QLabel(RealCentroid)
@@ -28,29 +28,33 @@ class Ui_RealCentroid(object):
         self.layerBox.setGeometry(QtCore.QRect(10, 30, 371, 31))
         self.layerBox.setObjectName(_fromUtf8("layerBox"))
         self.label_2 = QtGui.QLabel(RealCentroid)
-        self.label_2.setGeometry(QtCore.QRect(10, 70, 201, 21))
+        self.label_2.setGeometry(QtCore.QRect(10, 100, 201, 21))
         self.label_2.setText(QtGui.QApplication.translate("RealCentroid", "Output point on surface layer", None, QtGui.QApplication.UnicodeUTF8))
         self.label_2.setObjectName(_fromUtf8("label_2"))
         self.pointEdit = QtGui.QLineEdit(RealCentroid)
         self.pointEdit.setEnabled(False)
-        self.pointEdit.setGeometry(QtCore.QRect(10, 90, 291, 31))
+        self.pointEdit.setGeometry(QtCore.QRect(10, 120, 291, 31))
         self.pointEdit.setObjectName(_fromUtf8("pointEdit"))
         self.browseButton = QtGui.QPushButton(RealCentroid)
-        self.browseButton.setGeometry(QtCore.QRect(310, 90, 71, 31))
+        self.browseButton.setGeometry(QtCore.QRect(310, 120, 71, 31))
         self.browseButton.setText(QtGui.QApplication.translate("RealCentroid", "Browse", None, QtGui.QApplication.UnicodeUTF8))
         self.browseButton.setObjectName(_fromUtf8("browseButton"))
         self.addBox = QtGui.QCheckBox(RealCentroid)
-        self.addBox.setGeometry(QtCore.QRect(10, 130, 361, 26))
+        self.addBox.setGeometry(QtCore.QRect(10, 160, 361, 26))
         self.addBox.setText(QtGui.QApplication.translate("RealCentroid", "Add to map canvas", None, QtGui.QApplication.UnicodeUTF8))
         self.addBox.setObjectName(_fromUtf8("addBox"))
         self.cancelButton = QtGui.QPushButton(RealCentroid)
-        self.cancelButton.setGeometry(QtCore.QRect(310, 160, 71, 31))
+        self.cancelButton.setGeometry(QtCore.QRect(310, 190, 71, 31))
         self.cancelButton.setText(QtGui.QApplication.translate("RealCentroid", "Cancel", None, QtGui.QApplication.UnicodeUTF8))
         self.cancelButton.setObjectName(_fromUtf8("cancelButton"))
         self.okButton = QtGui.QPushButton(RealCentroid)
-        self.okButton.setGeometry(QtCore.QRect(220, 160, 71, 31))
+        self.okButton.setGeometry(QtCore.QRect(220, 190, 71, 31))
         self.okButton.setText(QtGui.QApplication.translate("RealCentroid", "Ok", None, QtGui.QApplication.UnicodeUTF8))
         self.okButton.setObjectName(_fromUtf8("okButton"))
+        self.selectedBox = QtGui.QCheckBox(RealCentroid)
+        self.selectedBox.setGeometry(QtCore.QRect(10, 70, 361, 26))
+        self.selectedBox.setText(QtGui.QApplication.translate("RealCentroid", "Selected features only", None, QtGui.QApplication.UnicodeUTF8))
+        self.selectedBox.setObjectName(_fromUtf8("selectedBox"))
 
         self.retranslateUi(RealCentroid)
         QtCore.QMetaObject.connectSlotsByName(RealCentroid)
